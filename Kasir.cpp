95353a0744bdab1bb04fabb10b3568303ab8fa3f
Febri <reynalfebry@gmail.com>
2026-03-08 22:51:07 +0700

    Update Kasir.cpp

--- Kasir.cpp
+++ Kasir.cpp
@@ -1,75 +1,84 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-int main(){
+int main() {
+    const int MAX_MENU = 100;
 
-    int pilihan;
-    int harga;
-    int jumlah;
-    int subtotal;
-    int totalsemua = 0;
-    string menu;
+    string daftarMenu[MAX_MENU] = {"Espresso", "Cappucino", "MatchaLatte", "Croissant", "Americano"};
+    int daftarHarga[MAX_MENU] = {15000, 20000, 15000, 12000, 15000};
+    int jumlahMenu = 5; // jumlah menu awal
 
-    //struk menu
+    // struk belanja
     string strukMenu[100];
     int strukJumlah[100];
     int strukSubtotal[100];
     int index = 0;
+    int totalsemua = 0;
 
     while(true){
-
         cout << "\n=== KOPI SANTAI ===" << endl;
-        cout << "1. Espresso - 15000" << endl;
-        cout << "2. Cappucino - 20000" << endl;
-        cout << "3. Matcha Latte - 15000" << endl;
-        cout << "4. Croissant - 12000" << endl;
-        cout << "5. Americano - 15000" << endl;
+        for(int i = 0; i < jumlahMenu; i++){
+            cout << i+1 << ". " << daftarMenu[i] << " - " << daftarHarga[i] << endl;
+        }
+        cout << jumlahMenu+1 << ". Tambah Menu Baru" << endl;
         cout << "0. Selesai" << endl;
 
+        int pilihan;
         cout << "Pilih menu : ";
         cin >> pilihan;
 
-        if(pilihan == 0){
-            break;
+        if(pilihan == 0) break;
+
+        // Tambah menu baru
+        if(pilihan == jumlahMenu + 1){
+            string menuBaru;
+            int hargaBaru;
+            cout << "Nama menu baru (tanpa spasi): ";
+            cin >> menuBaru;
+            cout << "Harga : ";
+            cin >> hargaBaru;
+
+            // simpan ke array
+            daftarMenu[jumlahMenu] = menuBaru;
+            daftarHarga[jumlahMenu] = hargaBaru;
+            jumlahMenu++;
+
+            // simpan ke file juga
+            ofstream fileTambah("menu.txt", ios::app);
+            fileTambah << menuBaru << " " << hargaBaru << endl;
+            fileTambah.close();
+
+            cout << "Menu berhasil ditambahkan!\n";
+            continue;
         }
 
-        switch(pilihan){
-            case 1: harga = 15000; menu = "Espresso"; break;
-            case 2: harga = 20000; menu = "Cappucino"; break;
-            case 3: harga = 15000; menu = "Matcha Latte"; break;
-            case 4: harga = 12000; menu = "Croissant"; break;
-            case 5: harga = 15000; menu = "Americano"; break;
+        // Pilih menu dari daftar
+        if(pilihan >= 1 && pilihan <= jumlahMenu){
+            int jumlah;
+            cout << "Jumlah : ";
+            cin >> jumlah;
 
-            default:
-                cout << "Menu tidak tersedia\n";
-                continue;
+            int subtotal = daftarHarga[pilihan-1] * jumlah;
+            totalsemua += subtotal;
+
+            cout << "Subtotal : " << subtotal << endl;
+
+            // simpan ke struk
+            strukMenu[index] = daftarMenu[pilihan-1];
+            strukJumlah[index] = jumlah;
+            strukSubtotal[index] = subtotal;
+            index++;
+        } else {
+            cout << "Menu tidak tersedia\n";
         }
-
-        cout << "Jumlah : ";
-        cin >> jumlah;
-
-        subtotal = harga * jumlah;
-        totalsemua += subtotal;
-
-        cout << "Subtotal : " << subtotal << endl;
-
-        // simpan transaksi ke struk
-        strukMenu[index] = menu;
-        strukJumlah[index] = jumlah;
-        strukSubtotal[index] = subtotal;
-        index++;
     }
 
-    cout << "\nTotal Belanja : " << totalsemua << endl;
-    cout << " " << endl;
-
-    cout << "=== STRUK BELANJA ===" << endl;
+    // Cetak struk
+    cout << "\n=== STRUK BELANJA ===" << endl;
     for(int i = 0; i < index; i++){
-        cout << strukMenu[i] << " x " << strukJumlah[i] 
-             << " = " << strukSubtotal[i] << endl;
+        cout << strukMenu[i] << " x " << strukJumlah[i] << " = " << strukSubtotal[i] << endl;
     }
-
-    cout << " " << endl;
     cout << "Total Belanja : " << totalsemua << endl;
 
     return 0;
